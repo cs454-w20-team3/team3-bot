19bfced622969849d6e5f2a1462d649d92687ba6
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2020-02-03 14:38:50 -0800

    Got a tiny bit of mocking working. should be able to get minimum coverage soon

--- test/team3player/RobotPlayerTest.java
+++ test/team3player/RobotPlayerTest.java
@@ -1,13 +1,41 @@
 package team3player;
-
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.Team;
+import battlecode.common.*;
 
 public class RobotPlayerTest {
 
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 	@Test
 	public void testSanity() {
 		assertEquals(2, 1+1);
+	}
+	RobotController rc;
+	@Test
+	public void simpleRCtest() {
+		// mock creation
+		// List mockedList = mock(List.class);
+		rc = mock(RobotController.class);
+		RobotInfo[] sense = new RobotInfo[0];
+		when(rc.getTeam()).thenReturn(Team.A);
+        when(rc.getType()).thenReturn(RobotType.HQ); // TODO?!
+        when(rc.getID()).thenReturn(0);
+        when(rc.getLocation()).thenReturn(new MapLocation(0, 0));
+		when(rc.getRoundNum()).thenReturn(0);
+		when(rc.senseNearbyRobots(anyInt(), any(Team.class))).thenReturn(new RobotInfo[]{});
+		RobotFramework robot = new MinerRobot(rc);
+		// verify(rcmock).senseNearbyRobots();
 	}
 
 }
