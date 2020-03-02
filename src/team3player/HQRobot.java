cc4ea51cad06146efbd588804c56a0cf8164a35b
Mi Yon Kim <youn0125@gmail.com>
2020-03-02 01:22:45 -0800

    HQ send spam messages(DSCreated, Soup location) to block chain. Sending HQ location doesn't confuse the enemy, so I didn't include this.

--- src/team3player/HQRobot.java
+++ src/team3player/HQRobot.java
@@ -3,22 +3,36 @@ import battlecode.common.*;
 class HQRobot extends RobotFramework {
     int numOfMiners = 0;
     final int maxNumOfMiners = 10;
-    int enemySecret = 444444444;
+    int enemySecret = -1;
 
     HQRobot(RobotController rc_) throws GameActionException {
         //super(rc_) calls the constructor of the parent class which just saves rc
         //the parent class also has the old utility functions like tryMove which need rc
         super(rc_);
-        sendHqMssg(enemySecret);
-        //enemySecret = getMsgFromBlockchain();
+
     }
     public void myTurn() throws GameActionException {
         waitforcooldown();
-
-        /*if (enemySecret != -1 && (rc.getRoundNum() % 10) == 1) {
-            System.out.println("I got secret code from BC: " + enemySecret);
-            sendHqMssg(enemySecret);
-        }*/
+        if(enemySecret == -1) {
+            enemySecret = getMsgFromBlockchain();
+            System.out.println("enemySecret: " + enemySecret);
+        }
+      else {
+            if( rc.getRoundNum() > 40 && rc.getRoundNum() < 140){
+                //sendHqMssg(enemySecret);
+                for(int i = 0; i < 3; i++)
+                    sendDSMssg(enemySecret);
+            }
+            if( rc.getRoundNum() > 5 && rc.getRoundNum() < 20){
+                //sendHqMssg(enemySecret);
+                for(int i = 0; i < 7; i++)
+                    sendSoupMssg(enemySecret);
+            }
+        }
+//        else {
+//            for(int i = 0; i < 7; i++)
+//                sendHqMssg(enemySecret);
+//        }
         for (RobotInfo bot : rc.senseNearbyRobots(-1, rc.getTeam().opponent())) {
             if (rc.canShootUnit(bot.ID)) {
                 rc.shootUnit(bot.ID);
